328022d243ffffaaa35550f96c9ba7d92d7cfead
alex <alexisetienne86@gmail.com>
2019-04-24 16:39:46 +0200

    first

---
+++ bleu122/src/main/java/bleu122/bleu122/Bleu122Application.java
@@ -0,0 +1,13 @@
+package bleu122.bleu122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bleu122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bleu122Application.class, args);
+	}
+
+}
---
+++ bleu122/src/test/java/bleu122/bleu122/Bleu122ApplicationTests.java
@@ -0,0 +1,16 @@
+package bleu122.bleu122;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bleu122ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
